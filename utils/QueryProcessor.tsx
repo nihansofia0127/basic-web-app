877fbae983668346d316bb7e300a6afd4ead3cb8
Sofia Yu <nihansofia0127@gmail.com>
2025-10-07 10:04:30 -0400

    sofiay

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Sofia Yu";
+    return "sofiay";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
